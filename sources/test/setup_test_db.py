88720dd06399e6d6cd6bc5dd6575976b37bc2636
Approximator <alex@nls.la>
2018-10-11 01:17:06 +0200

    Improved database setup useng faker
    Now test databese will contain a lot of users and
    each user has a lot budgets
    
    (refs #2)

--- sources/test/setup_test_db.py
+++ sources/test/setup_test_db.py
@@ -16,20 +16,38 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import datetime
+import hashlib
 from tornado_sqlalchemy import make_session_factory
 from sources.db.models import BASE_MODEL, User, Budget
+from faker import Faker
 
-
+fake = Faker('ru_RU')
 session_factory = make_session_factory('sqlite:////tmp/pbt_test.db')
 BASE_MODEL.metadata.create_all(session_factory.engine)
 
 session = session_factory.make_session()
 
-new_user = User(login='test_user', pwd_hash='test_pwd', username='test', user_pic='/some/pic')
-new_budget = Budget(owner=new_user, category='category', date=datetime.date.today(),
-                    title='something', amount=10, currency='UAH')
 
-session.add(new_user)
+def make_user():
+    return User(
+        login=fake.user_name(),
+        pwd_hash=hashlib.sha256(fake.password().encode()).hexdigest(),
+        username=fake.name(),
+        user_pic=fake.image_url())
+
+
+def make_budget(owner):
+    Budget(
+        owner=owner,
+        category=fake.word(),
+        date=fake.date_this_year(),
+        title=fake.catch_phrase(),
+        amount=fake.random_int(),
+        currency=fake.currency_code())
+
+users = [make_user() for _ in range(10)]
+
+for new_user in users:
+    budgets = [make_budget(new_user) for _ in range(fake.random_int(max=100))]
+    session.add(new_user)
 session.commit()
-
